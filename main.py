218d091d3550cd1cd5742fc8761e1278eb1e4811
patrick mugambi <patrick@kali.patrick>
2025-10-26 12:16:46 +0300

    define http requests

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 app = FastAPI()
+
+# Define a Pydantic model for post
+class Post(BaseModel):
+    title: str
+    content: str
+
 
 @app.get("/")
 async def root():
@@ -8,3 +15,8 @@ async def root():
 @app.get("/posts")
 async def get_posts():
     return {"data": "List of posts"}
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new post": f"title {payload[title]} {payload[content]}"}
