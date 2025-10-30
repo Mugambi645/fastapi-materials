166e2ede497f95710ce86fb5ba19751eb893cfa3
patrick mugambi <patrick@kali.patrick>
2025-10-30 12:31:06 +0300

    retrieve single item

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "i like pizza", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
@@ -27,3 +35,9 @@ def create_post(post: Post):
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(int(id))
+    return {"post detail": post}
